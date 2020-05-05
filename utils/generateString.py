8e3975a12dd6e248ed13b07d7b81e26b0523bb3d
unknown <vanquan.ajc@gmail.com>
2020-05-05 15:24:04 +0700

    Minor changes

--- utils/generateString.py
+++ utils/generateString.py
@@ -4,4 +4,4 @@ def generate_string(length=24):
     """
     Generate a <length> character string.
     """
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length)
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
